4f1e48f5389f2b8cab3dcac2dc1dcf90bab3966b
Gleb-1649 <gleb.popov0103@gmail.com>
2026-05-28 18:00:10 +0300

    feat: добавлен Tracking Service и настроена работа с сессиями

---
+++ GamePlatform/TrackingService/Data/TrackingDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackingService.Models;
+
+namespace TrackingService.Data;
+
+public class TrackingDbContext : DbContext
+{
+    public TrackingDbContext(DbContextOptions<TrackingDbContext> options) : base(options) { }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<Score> Scores { get; set; }
+}
---
+++ GamePlatform/TrackingService/Migrations/20260528145106_InitialTrackingCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTrackingCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Points = table.Column<int>(type: "integer", nullable: false),
+                    EarnedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DurationSeconds = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scores");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
